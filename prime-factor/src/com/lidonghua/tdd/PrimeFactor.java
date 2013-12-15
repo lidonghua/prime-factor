35d51981191b424640a1f020caf3bbe326d3f6a5
Li Donghua <donghua.li@dianping.com>
2013-12-15 22:48:45 +0800

    Simple approach works, again.

--- prime-factor/src/com/lidonghua/tdd/PrimeFactor.java
+++ prime-factor/src/com/lidonghua/tdd/PrimeFactor.java
@@ -8,8 +8,8 @@ public class PrimeFactor
     public List<Integer> factor(int number)
     {
         ArrayList<Integer> primeFactors = new ArrayList<>();
-        if (number == 2)
-            primeFactors.add(2);
+        if (number > 1)
+            primeFactors.add(number);
         return primeFactors;
     }
 }
